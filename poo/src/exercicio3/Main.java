196b15841d77cf4332d5f8f18e5a4634264e6851
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2024-09-05 23:09:52 +0000

    up final exercicio 3

--- poo/src/exercicio3/Main.java
+++ poo/src/exercicio3/Main.java
@@ -3,12 +3,6 @@ package exercicio3;
 public class Main {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa("Pessoa 1", 80.00, 1.73, 19);
-        System.out.println("Dados Originais: ");
-        System.out.println("nome: " + p1.nome);
-        System.out.println("peso: " + p1.peso);
-        System.out.println("altura: " + p1.altura);
-        System.out.println("idade: " + p1.idade);
-        System.out.println("-----------------------");
 
         p1.emagrecer();
         System.out.println("Novo peso: " + p1.peso);
