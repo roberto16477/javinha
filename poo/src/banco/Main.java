302a43ce974a6978c23bff1678efd4da9ca876a0
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2024-09-13 14:48:08 +0000

    up

--- poo/src/banco/Main.java
+++ poo/src/banco/Main.java
@@ -17,5 +17,29 @@ public class Main{
         conta2.creditar(2000);
         System.out.println("Seu saldo: " + conta2.getSaldo());
         System.out.println("----------------------------------------");
+
+        Poupanca conta3 = new Poupanca(3,1000, "Claudia",1000);
+        Investimento conta4 = new Investimento(4, 1000, "Cida", 8, 10);
+        Investimento conta5 = new Investimento(5, 1000, "Eva", 22, 5);
+
+        conta3.debitar(1500);
+        conta3.debitar(500);
+
+        System.out.println("Seu saldo: " + conta3.getSaldo());
+        System.out.println("Seu saldo mínimo: " + conta3.getSaldominimo());
+        conta3.creditar(1500);
+
+        System.out.println("Seu saldo: " + conta3.getSaldo());
+        System.out.println("Seu saldo mínimo: " + conta3.getSaldominimo());
+        conta3.atualizarSaldo();
+
+        System.out.println("Seu saldo: " + conta3.getSaldo());
+        System.out.println("Seu saldo mínimo: " + conta3.getSaldominimo());
+        System.out.println("----------------------------------------");
+
+        conta4.atualizarSaldo();
+        System.out.println("Seu saldo é: " + conta4.getSaldo());
+        conta5.atualizarSaldo();
+        System.out.println("Seu saldo é: " + conta5.getSaldo());
     }
 }
