ec52c05307521f004057397ba7c0c79a67c3c4c4
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2024-09-13 13:40:15 +0000

    up banco

---
+++ poo/src/banco/corrente.java
@@ -0,0 +1,31 @@
+public class corrente {
+    private int numero;
+    private double saldo;
+    private String cliente;
+
+    public void Corrente (int num, double sal,String cli){
+    this.numero = num;
+    this.saldo = sal;
+    this.cliente = cli;
+    }
+
+    public void creditar(double valor){
+    this.saldo = this.saldo + valor;
+    }
+
+    public void debitar(double valor){
+    if (valor <= this.saldo) {
+    this.saldo = this.saldo - valor;
+    } else {
+    System.out.println("Saldo Insuficiente");
+    }
+    }
+
+    public double getSaldo(){
+    return this.saldo;
+    }
+    public void setSaldo(double saldo){
+    this.saldo = saldo;
+    }
+
+}
